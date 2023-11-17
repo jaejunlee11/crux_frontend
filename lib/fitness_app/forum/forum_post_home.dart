446eee14fdeb5900b10253789cdab79d36c13c94
jaejunlee11 <sktks11@naver.com>
2023-11-17 16:19:19 +0900

    다시 복구

--- lib/fitness_app/forum/forum_post_home.dart
+++ lib/fitness_app/forum/forum_post_home.dart
@@ -2,107 +2,99 @@ import 'package:best_flutter_ui_templates/fitness_app/forum/forum_new_post_page.
 import 'package:flutter/material.dart';
 import 'tabs/post_tab.dart';
 import 'tabs/post_like_tab.dart';
-class ForumHomeScreen extends StatefulWidget {
 
-int docID = 0; //default id  
+class ForumHomeScreen extends StatefulWidget {
+  int docID = 0;
+
+  ForumHomeScreen({super.key}); //default id
 
   @override
   _ForumHomeScreenState createState() => _ForumHomeScreenState();
 }
 
-class _ForumHomeScreenState extends State<ForumHomeScreen> 
-with SingleTickerProviderStateMixin{  // 단일 애니메이션 위젯 정의 시 사용함
+class _ForumHomeScreenState extends State<ForumHomeScreen>
+    with SingleTickerProviderStateMixin {
+  // 단일 애니메이션 위젯 정의 시 사용함
   late TabController controller; //차후 확인 (late)
   int _currentTabIndex = 1;
   @override
-  void initState(){
+  void initState() {
     super.initState();
     controller = TabController(length: 3, vsync: this);
   }
 
-    void setDocID(int id) {
+  void setDocID(int id) {
     // Update the docID when called by the child widget
     setState(() {
       widget.docID = id;
     });
   }
 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('게시글 확인 / 자유 게시판'),
+        title: const Text('게시글 확인 / 자유 게시판'),
         backgroundColor: Colors.green,
-      actions: <Widget>[
-        Padding(
-          padding: const EdgeInsets.fromLTRB(0.0,0.0,40.0,0.0),
-        ),
-      ],
-      bottom: TabBar(
-      controller : controller,
-      tabs: <Widget>[
-        Tab(
-          child:InkWell(
-                onTap: () {
-                  setState((){
-                  _currentTabIndex=0;
-                  });
-                },
-                child: Icon(
-                    Icons.star_border,
-                    color: Colors.blue,
+        actions: const <Widget>[
+          Padding(
+            padding: EdgeInsets.fromLTRB(0.0, 0.0, 40.0, 0.0),
+          ),
+        ],
+        bottom: TabBar(
+          controller: controller,
+          tabs: const <Widget>[
+            Tab(
+              child: Icon(
+                Icons.star_border,
+                color: Colors.blue,
 //                    text: '모든글 보기'
-                  ),
-          ),
-        ),
-        Tab(
-          child:InkWell(
-                onTap: () {
-                  setState((){
-                    _currentTabIndex=1;                  
-                });
-                },
-                child: Icon(
-                    Icons.star,
-                    color: Colors.blue,
- //                   text: '추천글 보기'
-                  ),
-          ),
-        ),
-        Tab(
-          child:InkWell(
-                onTap: () {                
-                  Navigator.push(
+              ),
+            ),
+            Tab(
+              child: Icon(
+                Icons.star,
+                color: Colors.blue,
+                //                   text: '추천글 보기'
+              ),
+            ),
+            Tab(
+              child: Icon(
+                Icons.create,
+                color: Colors.black,
+                //                   text: '작성하기'
+              ),
+            ),
+          ],
+          onTap: (index) async {
+            if (index == 2) {
+              await Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => NewPostScreen()));},
-                child: Icon(
-                    Icons.create,
-                    color: Colors.black,
- //                   text: '작성하기'
-                  ),
-          ),
+                  MaterialPageRoute(
+                      builder: (context) => const NewPostScreen()));
+            } else {
+              setState(() {
+                _currentTabIndex = index;
+              });
+            }
+          },
         ),
-      ],
-      ),
       ),
       body: TabBarView(
         controller: controller,
-        physics: NeverScrollableScrollPhysics(),
+        physics: const NeverScrollableScrollPhysics(),
         children: <Widget>[
           _currentTabIndex == 0
-          ? ForumPostTab(onTapCallback: setDocID)
-          : Container(),
+              ? ForumPostTab(onTapCallback: setDocID)
+              : Container(),
           _currentTabIndex == 1
-          ? ForumLikeTab(onTapCallback: setDocID)
-          : Container(),
-
+              ? ForumLikeTab(onTapCallback: setDocID)
+              : Container(),
         ],
       ),
-          );
-     
+    );
   }
 }
-
-
 
 /*
 import 'package:best_flutter_ui_templates/fitness_app/forum/forum_new_post_page.dart';
